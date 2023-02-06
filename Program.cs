7c27703dfd9c2a600f5c67559548ff8331023b2a
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-02-05 22:36:15 -0300

    aula 2: Results/Responses, OAS Documentation, Named Endpoints

--- Program.cs
+++ Program.cs
@@ -1,49 +1,44 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<GreetingService>();
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
-app.MapGet("/test", ([AsParameters] PaginationDto dto) => dto);
+// app.MapGet("/test/simple", (int id) => 
+//     id > 10 ? 
+//         Results.Ok(new Resp() { Message = "Ok", IsSuccess = true })
+//         : Results.NotFound()
+// )
+//     .Produces<ProblemDetails>(StatusCodes.Status200OK)
+//     .Produces(StatusCodes.Status404NotFound)
+//     .WithOpenApi(operation => new(operation){
+//         Summary = "Just a test Summary",
+//         Description = "Just a test description"
+//     });
+
+app.MapGet("/api/msgs/{id:int}", (int id) => 
+    TypedResults.Ok(new Msg(){ Id = id, Message = "Test" }))
+    .WithName("GetMsgDetail");
+
+app.MapPost("/api/msgs", (Msg msg) => 
+    TypedResults.CreatedAtRoute(msg, "GetMsgDetail", new { Id = msg.Id }))
+    .WithName("CreateMessage");
+
+app.MapGet("/test", (LinkGenerator linkGen) => new {
+    CreateMsgRoute = linkGen.GetPathByName("CreateMessage"),
+    GetMsgDetail = linkGen.GetPathByName("GetMsgDetail", new{ Id = 10 })
+});
 
 app.Run();
 
-class GreetingService {
-    public string Greet(string name) => $"Hello, {name}!";
-}
-
-record PaginationDto(int CurrentPage, int PerPage);
-
-record Todo(int Id, string Title)
-{
-    // public static bool TryParse(string todoEncoded, out Todo? todo) 
-    // {
-    //     try{
-    //         var parts = todoEncoded.Split(",");
-    //         todo = new Todo(int.Parse(parts[0]), parts[1]);
-    //         return true;
-    //     } catch(Exception _)
-    //     {
-    //         todo = null;
-    //         return false;
-    //     }
-    // }
-
-    // public static async ValueTask<Todo?> BindAsync(
-    //     HttpContext context,
-    //     ParameterInfo parameterInfo
-    // ){
-    //     try{
-    //         var content = await new StreamReader(context.Request.Body).ReadToEndAsync();
-    //         var parts = content.Split(",");
-    //         return new Todo(int.Parse(parts[0]), parts[1]);
-    //     } catch(Exception _)
-    //     {
-    //         return null;
-    //     }
-    // }
+class Msg {
+    public int Id { get; set; }
+    public required string Message { get; set; }
 }
